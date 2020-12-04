e78b53db8b3177183391b68488239139f7cb210e
Stefano Franchini <stefano.franchini77@gmail.com>
2020-12-04 16:21:21 +0100

    add buttons

--- js/script.js
+++ js/script.js
@@ -4,9 +4,28 @@ let app = new Vue (
         data: {
             firstMessage: "forza",
             secondMessage: "inter",
-            titleClass: "title",
-            spanClass: "blue",
+            firstClass: "black",
+            secondClass: "blue",
             myImage: "https://www.fcinter1908.it/wp-content/uploads/sites/45/2018/09/san-siro-meazza-tifosi-inter-curva.jpg",
-        }
+        },
+        methods: {
+            changeElements: function() {
+                if (this.firstClass == "black") {
+
+                    this.myImage = "https://images.daznservices.com/di/library/GOAL/eb/bf/inter-milan-serie-a_816vz4syxnhn1uicjsyaidhx8.jpg?t=557317767&quality=100";
+
+                    const classContainer = this.firstClass;
+                    this.firstClass = this.secondClass;
+                    this.secondClass = classContainer;
+                } else {
+
+                    this.myImage = "https://www.fcinter1908.it/wp-content/uploads/sites/45/2018/09/san-siro-meazza-tifosi-inter-curva.jpg";
+
+                    const classContainer = this.firstClass;
+                    this.firstClass = this.secondClass;
+                    this.secondClass = classContainer;
+                }
+            }
+        },
     }
 );
