6929b4f47e7f08b9a11ab31966153787fdb847ff
Stefano Franchini <stefano.franchini77@gmail.com>
2020-12-04 15:56:16 +0100

    title & img

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("ciao")
+let app = new Vue (
+    {
+        el: "#root",
+        data: {
+            firstMessage: "forza",
+            secondMessage: "inter",
+            titleClass: "title",
+            spanClass: "blue",
+            myImage: "https://www.fcinter1908.it/wp-content/uploads/sites/45/2018/09/san-siro-meazza-tifosi-inter-curva.jpg",
+        }
+    }
+);
